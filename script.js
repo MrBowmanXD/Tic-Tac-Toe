0c9edf84a244fa913b74fe96031faedff562f5fa
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2021-02-14 02:27:15 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const GameBoard = {
+  gameboard: [],
+  flow: [],
+};
+
+const displayController = {
+  displayController: "display?",
+};
+
+const player = {
+  name: "name",
+  player: 1,
+  turn: true,
+};
